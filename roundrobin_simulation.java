b7231dc5a69b1cbab6fa8822023b2b9987ec8767
Doknotknock111 <126953822+Doknotknock111@users.noreply.github.com>
2023-04-02 19:54:46 +0530

    Update roundrobin_simulation.java

--- roundrobin_simulation.java
+++ roundrobin_simulation.java
@@ -22,12 +22,13 @@ public class Main {
     }
     
     static class Scheduler {
-        int timekimit;
+        int timeLimit;
         int quantum;
         Queue<Process> queue;
         
-        Scheduler(int q) {
+        Scheduler(int q, int t) {
             quantum = q;
+            timeLimit = t;
             queue = new LinkedList<>();
         }
         
@@ -45,7 +46,7 @@ public class Main {
         
         void roundRobin() {
             int currentTime = 0;
-            while (!isEmpty()) {
+            while (!isEmpty() && currentTime < timeLimit) {
                 Process p = removeProcess();
                 int timeSlice = Math.min(quantum, p.remainingTime);
                 p.remainingTime -= timeSlice;
@@ -61,34 +62,38 @@ public class Main {
     }
     
     public static void main(String[] args) {
-             
+        Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        System.out.print("Enter the number of processes: ");
-        Scanner sc = new Scanner(System.in);
-        int numProcesses = sc.nextInt();
+       
+        // read input from user
+        System.out.print("Enter number of processes: ");
+        int numProcesses = scanner.nextInt();
+        System.out.print("Enter time quantum: ");
+        int quantum = scanner.nextInt();
+        System.out.print("Enter the time limit: ");
+        int timeLimit = scanner.nextInt();
+        
         List<Process> processes = new ArrayList<>();
         for (int i = 0; i < numProcesses; i++) {
             int arrivalTime = random.nextInt(100);
             int burstTime = random.nextInt(numProcesses) + 1;
             processes.add(new Process(i, arrivalTime, burstTime));
         }
-        System.out.print("Enter the time quantum: ");
-         int quantum = sc.nextInt();
-        Scheduler scheduler = new Scheduler(quantum);
+        
+        Scheduler scheduler = new Scheduler(quantum, timeLimit);
         for (Process p : processes) {
             scheduler.addProcess(p);
         }
         scheduler.roundRobin();
         
-        
         // Print results
         double totalWaitingTime = 0.0;
         double totalTurnaroundTime = 0.0;
         for (Process p : processes) {
-        System.out.printf("Process %d: arrivalTime=%d, burstTime=%d, waitingTime=%d, turnaroundTime=%d\n",
-        p.processID, p.arrivalTime, p.burstTime, p.waitingTime, p.turnaroundTime);
-        totalWaitingTime += p.waitingTime;
-        totalTurnaroundTime += p.turnaroundTime;
+            System.out.printf("Process %d: arrivalTime=%d, burstTime=%d, waitingTime=%d, turnaroundTime=%d\n",
+                    p.processID, p.arrivalTime, p.burstTime, p.waitingTime, p.turnaroundTime);
+            totalWaitingTime += p.waitingTime;
+            totalTurnaroundTime += p.turnaroundTime;
         }
         double avgWaitingTime = totalWaitingTime / numProcesses;
         double avgTurnaroundTime = totalTurnaroundTime / numProcesses;
@@ -96,6 +101,3 @@ public class Main {
         System.out.printf("Average turnaround time: %.2f\n", avgTurnaroundTime);
     }
 }
-// take user input for the time limit and keep track of the processes and also
-//if any process was not completed as the time was over then print the process which was not complete
-//then print the unfinished process with the time left .
